566f8e7b57b66c53293b3fc509822257cdecab75
Kirill Kuznetsov <kirillkuz97@gmail.com>
2020-08-06 21:55:17 +0300

    Create Application for sort array

---
+++ SortApp/ArrayClass.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortApp
+{
+    class ArrayClass
+    {
+        public int[] array { get; set; }
+        public int Length
+        {
+            get { return array.Length; }            
+        }
+        
+        private Random random;
+        public ArrayClass(int leng)
+        {
+            array = new int[leng];
+            random = new Random(1);
+            ArrayGenerate();
+        }
+        public void Print()
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0:D3} ", array[i]);
+                if (i % 10 == 9)
+                    Console.WriteLine();
+            }
+            Console.WriteLine();
+            System.Threading.Thread.Sleep(100);
+        }
+        public void Print(int q)
+        {
+            Console.WriteLine("Иттерация №" + q.ToString());
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0:D3} ", array[i]);
+                if (i % 10 == 9)
+                    Console.WriteLine();
+            }
+            Console.WriteLine();
+            System.Threading.Thread.Sleep(100);
+        }
+        public static void Print(int[] ar)
+        {
+            for (int i = 0; i < ar.Length; i++)
+            {
+                Console.Write("{0:C3}",ar[i]);
+                if (i % 10 == 9)
+                    Console.WriteLine();
+            }
+        }
+        public void ArrayGenerate()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 1000);
+            }
+        }
+        public void Swap(int i, int j)
+        {
+            int a = array[i];
+            array[i] = array[j];
+            array[j] = a;
+        }
+        public int this[int i]
+        {
+            get
+            {
+                return array[i];
+            }
+        }
+        
+        
+    }
+}
--- SortApp/Program.cs
+++ SortApp/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 
+
 namespace SortApp
 {
     class Program
     {
+        static ArrayClass ar;
+        static Random random;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            random = new Random(1);
+            ar = new ArrayClass(10);
+            Console.WriteLine("Начальный массив");
+            ar.Print();
+
+            BubleSort(ar);
+            Console.ReadKey();
         }
+
+        public static void NotBubleSort(ArrayClass a)
+        {
+            for(int i=0;i<a.Length;i++)
+            {
+                for (int j = 0; j < a.Length; j++)
+                {
+                    if (a[i] > a[j])
+                        a.Swap(i, j);                    
+                    a.Print(i * a.Length + j + 1);
+                }
+            }
+        }
+        public static void BubleSort(ArrayClass a)
+        {
+            bool swapped;
+            int q = 0;
+            do
+            {
+                swapped = false;
+                for (int i = 1; i < a.Length; i++)
+                {
+                    if (a[i] < a[i - 1])
+                    {
+                        a.Swap(i, i - 1);
+                        swapped = true;
+                    }
+                    a.Print(++q);
+                }
+            }
+            while (swapped);
+        }
+        
+
     }
 }
