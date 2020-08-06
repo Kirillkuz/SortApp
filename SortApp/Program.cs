a12773163919b0cb55ac675c88e028e26676dcb0
Kirill Kuznetsov <kirillkuz97@gmail.com>
2020-08-06 19:39:41 +0300

    Добавьте файлы проекта.

---
+++ SortApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SortApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
